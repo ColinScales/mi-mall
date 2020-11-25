edf1c4f8200d5824407bbd8300ec8c8045c3965b
tooann <1477835701@qq.com>
2020-11-26 02:49:30 +0800

    修改接口地址

--- env.js
+++ env.js
@@ -1,11 +1,18 @@
+/*
+ * @Description: file content
+ * @Author: sunkl
+ * @Date: 2020-10-29 00:40:38
+ * @LastEditors: sunkl
+ * @LastEditTime: 2020-11-26 02:48:49
+ */
 let baseUrl = ''
 
 switch (process.env.NODE_ENV) {
   case 'development':
-    baseUrl = 'sdsd'
+    baseUrl = '/mi-product'
     break
   case 'production':
-    baseUrl = 'sdsd'
+    baseUrl = '/mi-product'
     break
 }
 
